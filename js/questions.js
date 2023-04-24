e40222207c87ab2a8dfff2185feedfa1d80888b5
Votre imed ben amor <imed.7@outlook.fr>
2023-04-24 14:05:34 +0200

    cinquieme étape

--- js/questions.js
+++ js/questions.js
@@ -10,38 +10,54 @@ const responses = [
     3
 ];
 
-// Etape 1, on recupere la premiere question du tableau
-const question1 = questions[0];
-// Puis on l'affiche
-console.log(question1);
+// Etape 5
+// Ici on créer la methode askQuestion qui prend en parametre l'index de la question qu'on va poser et qui va retourner la réponse de l'utilisateur
+function askQuestion(questionIndex) {
+    // On pose une question à l'utilisateur et on récupère sa réponse
+    const userAnswer = window.prompt(questions[questionIndex]);
 
-// Etape 2, on utilise la methode window.prompt()
-// C'est une methode qui permet d'afficher une fenêtre avec un text input pour ensuite récupérer le retour de ce text input
-// Ici on affiche une fenetre ou sera affiché la question1, puis on récupère le retour du text input dans reponse1
-// Donc ici reponse1 sera egal à la réponse de l'utilisateur
-const reponse1 = window.prompt(question1);
-// Ici, on affiche le retour de l'utilisateur (reponse1)
-console.log(reponse1);
-
-// Etape 3, ici je check si la reponse reçu par l'utilisateur est bien egal a la premiere valeur de mon tableau responses (l'index numero [0] = premiere valeur du tableau)
-// Si c'est egal, on affiche CORRECT
-if (reponse1 == responses[0]) {
-    console.log("CORRECT");
-    // Etape 4, si bonne reponse de l'utilisateur, alors on ajoute la question 1 dans la liste vert qu'il y a à gauche
-    const container = document.querySelector("#right .responses");
-    // Ici on va ajouter dans le HTML l'élément qu'on veut ajouter
-    // Si on avait mit : 
-    // container.innerHTML = '<li>'+question1+'</li>';
-    // Ca aurait écraser tout le HTML deja existant
-    container.innerHTML = container.innerHTML + '<li>'+question1+'</li>';
+    // On retourne la réponse entrée par l'utilisateur
+    return userAnswer;
 }
 
-// Sinon, on affiche faux
-else {
-    // Ici meme process que si la reponse est correct, sauf qu'ici on gere le cas ou la reponse est fausse
-    // Donc ici on ajoute la question a la liste rouge a droite
-    const container = document.querySelector("#wrong .responses");
-    container.innerHTML = container.innerHTML + '<li>'+question1+'</li>';
-    console.log("faux");
+// Ici on créer une fonction nommée checkResponse qui va vérifier la réponse de l'utilisateur à la question dont l'index est passé en argument
+// - responseIndex correspond a l'index de la question
+// - answer correspond a la reponse (en string)
+// Si la reponse est correcte, checkResponse retourne un booleen : true si bonne reponse, false si mauvaise reponse
+function checkResponse(responseIndex, answer) {
+    if (answer == responses[responseIndex]) {
+        // Bonne reponse
+        return true;
+    }
+    // Mauvaise reponse 
+    else {
+        return false;
+    }
 }
 
+// Fonction qui ajoute une question dans la bonne liste selon la reponse de l'utilisateur
+// questionIndex pour savoir quelle question ajouter 
+// isCorrect, pour savoir si on a eu une bonne reponse ou non
+function addResponseToDom(questionIndex, isCorrect) {
+    // On créer une variable qui va stocker le container dans lequel on ajoutera notre question
+    // On la créer ici car si on l'avait créée dans le if ou le else, elle ne serait pas accessible apres le if/else à cause de la portée des variables avec let
+    let container;
+    // Si la reponse de l'utilisateur est correcte, la variable container contient l'élément ul de right, sinon celui de wrong
+    if (isCorrect === true) {
+        container = document.querySelector("#right .responses");
+    }
+    else {
+        container = document.querySelector("#wrong .responses");
+    }
+    // On vient ajouter un nouvel <li> contenant la question a notre container
+    container.innerHTML = container.innerHTML + '<li>'+questions[questionIndex]+'</li>';
+}
+
+// Pose la premère question et récupère la réponse de l'utilisateur
+let response = askQuestion(0);
+
+// Vérifie que la réponse donnée est bien la bonne
+let isCorrect = checkResponse(0, response);
+
+// Modifie le DOM (#right ou #wrong si c'est une bonne ou mauvaise réponse)
+addResponseToDom(0, isCorrect);
