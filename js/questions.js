f46e5a1f355e549cf9566c2787eb483e47ac6396
Votre imed ben amor <imed.7@outlook.fr>
2023-04-24 10:38:12 +0200

    deuxieme étape

--- js/questions.js
+++ js/questions.js
@@ -14,3 +14,11 @@ const responses = [
 const question1 = questions[0];
 // Puis on l'affiche
 console.log(question1);
+
+// Etape 2, on utilise la methode window.prompt()
+// C'est une methode qui permet d'afficher une fenêtre avec un text input pour ensuite récupérer le retour de ce text input
+// Ici on affiche une fenetre ou sera affiché la question1, puis on récupère le retour du text input dans reponse1
+// Donc ici reponse1 sera egal à la réponse de l'utilisateur
+const reponse1 = window.prompt(question1);
+// Ici, on affiche le retour de l'utilisateur (reponse1)
+console.log(reponse1);
